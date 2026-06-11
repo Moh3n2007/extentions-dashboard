62e5bb9c5a91875bffacef1fbe85aa4458a6e88c
Maryam <m.miraei94@gmail.com>
2026-06-11 18:30:09 +0330

    v5.1

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,12 +1,12 @@
 const Main=()=>{
     return (
       <>
-        <div className="flex justify-between">
-          <h1>
-            Extensions List <span>(12)</span>
+        <div className="flex justify-between p-">
+          <h1 className="text-3xl font-bold">
+            Extensions List <span className="text-text-200">(12)</span>
           </h1>
-          <div>
-            <button>All</button>
+          <div className="flex justify-between gap-4 ">
+            <button className="bg-bg-100 border-[1px] border-bg-100 hover:bg-bg-red">All</button>
             <button>Active <span>(8)</span></button>
             <button>Inactive <span>(4)</span></button>
           </div>
