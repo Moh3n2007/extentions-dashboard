4516f0a16047e32ad2049abe15e4ed5037cc1949
Maryam <m.miraei94@gmail.com>
2026-06-11 18:47:03 +0330

    hasti2

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,18 +1,23 @@
-const Main=()=>{
-    return (
-      <>
-        <div className="flex justify-between p-">
-          <h1 className="text-3xl font-bold">
-            Extensions List <span className="text-text-200">(12)</span>
-          </h1>
-          <div className="flex justify-between gap-4 ">
-            <button className="bg-bg-100 border-[1px] border-bg-100 hover:bg-bg-red w-16 h-12 rounded-4xl ">All</button>
-            <button>Active <span>(8)</span></button>
-            <button>Inactive <span>(4)</span></button>
-          </div>
+const Main = () => {
+  return (
+    <>
+      <div className="flex justify-between my-6 m">
+        <h1 className="text-3xl font-bold">
+          Extensions List <span className="text-text-200">(12)</span>
+        </h1>
+        <div className="flex justify-between gap-4 ">
+          <button className="bg-bg-100 border-[1px] border-bg-100 hover:bg-bg-red w-16 h-12 rounded-4xl ">
+            All
+          </button>
+          <button className="bg-bg-100 border-[1px] border-bg-100 hover:bg-bg-red w-16 h-12 rounded-4xl ">
+            Active <span>(8)</span>
+          </button>
+          <button className="bg-bg-100 border-[1px] border-bg-100 hover:bg-bg-red w-16 h-12 rounded-4xl ">
+            Inactive <span>(4)</span>
+          </button>
         </div>
-      </>
-    );
-    
+      </div>
+    </>
+  );
 };
 export default Main;
