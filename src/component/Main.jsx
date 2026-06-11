105880d10a026ea9ee836c4ff8527c6f7e87060a
Maryam <m.miraei94@gmail.com>
2026-06-11 18:13:21 +0330

    hasti1

---
+++ src/component/Main.jsx
@@ -0,0 +1,18 @@
+const Main=()=>{
+    return (
+      <>
+        <div className="flex justify-between">
+          <h1>
+            Extensions List <span>(12)</span>
+          </h1>
+          <div>
+            <button>All</button>
+            <button>Active <span>(8)</span></button>
+            <button>Inactive <span>(4)</span></button>
+          </div>
+        </div>
+      </>
+    );
+    
+};
+export default Main;
